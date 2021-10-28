1658b7c8868ef41e16e42b9ac85d20061a89716a
MRUNGP2 App <mrungp2app@gmail.com>
2021-10-28 14:39:11 +0800

    React to User Interaction with Java

--- app/src/main/java/com/bk/neweraoctober2021/MainActivity.java
+++ app/src/main/java/com/bk/neweraoctober2021/MainActivity.java
@@ -1,14 +1,36 @@
 package com.bk.neweraoctober2021;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private EditText edtEmail, edtPassword;
+    private Button btnLogin, btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
+        findViews();
+        setListeners();
+    }
+
+    private void findViews(){
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+    }
+
+    private void setListeners(){
+        btnLogin.setOnClickListener(view -> {
+            String email = edtEmail.getText().toString();
+
+            Toast.makeText(this, email, Toast.LENGTH_SHORT).show();
+        });
     }
 }
