6f24509b39204298c1b039444f440af8c76d830d
MRUNGP2 App <mrungp2app@gmail.com>
2021-12-16 14:32:20 +0800

    Contact SQLiteExample: Delete and Update

--- app/src/main/java/com/bk/neweraoctober2021/SQLiteDemo/EditContactActivity.java
+++ app/src/main/java/com/bk/neweraoctober2021/SQLiteDemo/EditContactActivity.java
@@ -1,16 +1,57 @@
 package com.bk.neweraoctober2021.SQLiteDemo;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bk.neweraoctober2021.R;
 
 public class EditContactActivity extends AppCompatActivity {
+    private EditText edtName, edtNumber;
+    private Button btnSave;
+    private Contact contact = new Contact();
+    private ContactDBHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_contact);
+        helper = new ContactDBHelper(this);
+        getDataFromExtra();
+        findViews();
+        setListeners();
+    }
+
+    private void getDataFromExtra(){
+        int id = getIntent().getExtras().getInt("id");
+        String name = getIntent().getExtras().getString("name");
+        String number = getIntent().getExtras().getString("number");
+
+        contact.setId(id);;
+        contact.setName(name);
+        contact.setNumber(number);
+    }
+
+    private void findViews(){
+        edtName = findViewById(R.id.edtName);
+        edtNumber = findViewById(R.id.edtNumber);
+        btnSave = findViewById(R.id.btnSave);
+
+        edtName.setText(contact.getName());
+        edtNumber.setText(contact.getNumber());
+    }
+
+    private void setListeners(){
+        btnSave.setOnClickListener(view -> {
+            contact.setName(edtName.getText().toString());
+            contact.setNumber(edtNumber.getText().toString());
+
+            helper.updateContact(contact);
+            Toast.makeText(this, "Contact Updated", Toast.LENGTH_SHORT).show();
+            finish();
+        });
     }
 }
--- app/src/main/java/com/bk/neweraoctober2021/SQLiteDemo/ViewContactActivity.java
+++ app/src/main/java/com/bk/neweraoctober2021/SQLiteDemo/ViewContactActivity.java
@@ -1,9 +1,12 @@
 package com.bk.neweraoctober2021.SQLiteDemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bk.neweraoctober2021.R;
@@ -12,11 +15,13 @@ public class ViewContactActivity extends AppCompatActivity {
     private TextView tvName, tvNumber;
     private Button btnDelete, btnEdit;
     private Contact contact = new Contact();
+    private ContactDBHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_contact);
+        helper = new ContactDBHelper(this);
         getContactFromExtra();
         findViews();
         setListeners();
@@ -43,6 +48,27 @@ public class ViewContactActivity extends AppCompatActivity {
     }
 
     private void setListeners(){
+        btnEdit.setOnClickListener( view -> {
+            Intent intent = new Intent(ViewContactActivity.this, EditContactActivity.class);
+            intent.putExtra("id", contact.getId());
+            intent.putExtra("name", contact.getName());
+            intent.putExtra("number", contact.getNumber());
+            startActivity(intent);
+            finish();
+        });
 
+        btnDelete.setOnClickListener( view -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setTitle("Delete Contact")
+                    .setMessage("Are you sure to delete " + contact.getName() + " from your contact?")
+                    .setPositiveButton("OK", (dialogInterface, i) -> {
+                        helper.deleteContact(contact.getId());
+                        Toast.makeText(this, "Contact Deleted", Toast.LENGTH_SHORT).show();
+                        finish();
+                    })
+                    .setNegativeButton("Cancel", null)
+            .show();
+        });
     }
 }
